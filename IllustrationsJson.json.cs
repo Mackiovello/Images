8e271cc86d549465a8c85998a37e4a02317a3703
miyconst <const.miyang@gmail.com>
2015-04-07 19:41:25 +0300

    Add confirmation on image delete
    https://github.com/Polyjuice/Images/issues/2

--- IllustrationsJson.json.cs
+++ IllustrationsJson.json.cs
@@ -1,32 +1,52 @@
+using System;
 using Starcounter;
 
 namespace Images {
     partial class IllustrationsJson : Json {
+        public Action ConfirmAction = null;
 
         public QueryResultRows<Simplified.Ring1.Illustration> _Illustrations {
-
             get {
                 return Db.SQL<Simplified.Ring1.Illustration>("SELECT o FROM Simplified.Ring1.Illustration o");
             }
         }
 
         void Handle(Input.Add action) {
-
-
             Db.Transact(() => {
                 Simplified.Ring1.Illustration i = new Simplified.Ring1.Illustration();
                 i.Concept = new Simplified.Ring1.Something();
                 i.Content = new Simplified.Ring1.Content();
             });
-
-
         }
-
-
+        
         public override string GetHtmlPartialUrl() {
             return Html;
         }
 
-  
+        [IllustrationsJson_json.Confirm]
+        partial class PersonsConfirmPage : Page {
+            void Cancel() {
+                this.ParentPage.Confirm.Message = null;
+                this.ParentPage.ConfirmAction = null;
+            }
+
+            void Handle(Input.Reject Action) {
+                Cancel();
+            }
+
+            void Handle(Input.Ok Action) {
+                if (this.ParentPage.ConfirmAction != null) {
+                    this.ParentPage.ConfirmAction();
+                }
+
+                Cancel();
+            }
+
+            public IllustrationsJson ParentPage {
+                get {
+                    return this.Parent as IllustrationsJson;
+                }
+            }
+        }
     }
 }
--- JSON/IllustrationJson.json.cs
+++ JSON/IllustrationJson.json.cs
@@ -2,15 +2,24 @@ using Starcounter;
 
 namespace Images.JSON {
     partial class IllustrationJson : Page, IBound<Simplified.Ring1.Illustration> {
-
         void Handle(Input.Delete action) {
+            this.ParentPage.ConfirmAction = () => {
+                Db.Transact(() => {
+                    this.Data.Delete();
+                });
+            };
 
-            this.Data.Delete();
-            this.Transaction.Commit();
+            this.ParentPage.Confirm.Message = "Are you sure want to delete image [" + this.Data.Concept.Name + "]?";
         }
 
         void Handle(Input.Name action) {
             this.Data.Concept = Db.SQL<Simplified.Ring1.Something>("SELECT o FROM Simplified.Ring1.Something o WHERE Name=?", action.Value).First;
+        }
+
+        IllustrationsJson ParentPage {
+            get {
+                return this.Parent.Parent as IllustrationsJson;
+            }
         }
     }
 }
