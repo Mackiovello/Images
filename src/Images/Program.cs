03380a8e8fd1af030d830924b7612cd5ebcb933b
aloscha <oleksiy.yatsko@gmail.com>
2016-04-26 20:14:57 +0200

    Remove from CommitHooks #9

--- src/Images/Program.cs
+++ src/Images/Program.cs
@@ -1,17 +1,13 @@
-    using Images.Api;
-
 namespace Images {
     class Program {
         static void Main() {
             UploadHandlers upload = new UploadHandlers();
             MainHandlers main = new MainHandlers();
             DefaultStyles styles = new DefaultStyles();
-            CommitHooks hooks = new CommitHooks();
 
             main.Register();
             upload.Register();
             styles.ApplyIfEmpty();
-            hooks.Register();
         }
     }
 }
