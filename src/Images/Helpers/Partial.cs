06b381f3ac5483efcf1a1096caf17405ba0ea73b
aloscha <oleksiy.yatsko@gmail.com>
2016-05-10 15:35:15 +0200

    Add default selfonly option for partials #3538

--- src/Images/Helpers/Partial.cs
+++ src/Images/Helpers/Partial.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Web.Security;
 using Starcounter;
 
 namespace Images.Helpers
@@ -13,61 +14,73 @@ namespace Images.Helpers
         #region Handle.Get replacement
         public static void Register(string uriTemplate, Func<Response> code, HandlerOptions ho = null)
         {
+            AddSelfOnlyOption(ref ho);
             Handle.GET(uriTemplate, code, ho);
         }
 
         public static void Register(ushort port, string uriTemplate, Func<Response> code, HandlerOptions ho = null)
         {
+            AddSelfOnlyOption(ref ho);
             Handle.GET(port, uriTemplate, code, ho);
         }
 
         public static void Register<T>(string uriTemplate, Func<T, Response> code, HandlerOptions ho = null)
         {
+            AddSelfOnlyOption(ref ho);
             Handle.GET(uriTemplate, code, ho);
         }
 
         public static void Register<T>(ushort port, string uriTemplate, Func<T, Response> code, HandlerOptions ho = null)
         {
+            AddSelfOnlyOption(ref ho);
             Handle.GET(port, uriTemplate, code, ho);
         }
 
         public static void Register<T1, T2>(string uriTemplate, Func<T1, T2, Response> code, HandlerOptions ho = null)
         {
+            AddSelfOnlyOption(ref ho);
             Handle.GET(uriTemplate, code, ho);
         }
 
         public static void Register<T1, T2>(ushort port, string uriTemplate, Func<T1, T2, Response> code, HandlerOptions ho = null)
         {
+            AddSelfOnlyOption(ref ho);
             Handle.GET(port, uriTemplate, code, ho);
         }
 
         public static void Register<T1, T2, T3>(string uriTemplate, Func<T1, T2, T3, Response> code, HandlerOptions ho = null)
         {
+            AddSelfOnlyOption(ref ho);
             Handle.GET(uriTemplate, code, ho);
         }
 
         public static void Register<T1, T2, T3>(ushort port, string uriTemplate, Func<T1, T2, T3, Response> code, HandlerOptions ho = null)
         {
+            AddSelfOnlyOption(ref ho);
             Handle.GET(port, uriTemplate, code, ho);
         }
 
         public static void Register<T1, T2, T3, T4>(string uriTemplate, Func<T1, T2, T3, T4, Response> code, HandlerOptions ho = null)
         {
+            AddSelfOnlyOption(ref ho);
             Handle.GET(uriTemplate, code, ho);
         }
 
         public static void Register<T1, T2, T3, T4>(ushort port, string uriTemplate, Func<T1, T2, T3, T4, Response> code, HandlerOptions ho = null)
         {
+            AddSelfOnlyOption(ref ho);
             Handle.GET(port, uriTemplate, code, ho);
         }
 
         public static void Register<T1, T2, T3, T4, T5>(string uriTemplate, Func<T1, T2, T3, T4, T5, Response> code, HandlerOptions ho = null)
         {
+            AddSelfOnlyOption(ref ho);
             Handle.GET(uriTemplate, code, ho);
         }
 
         public static void Register<T1, T2, T3, T4, T5>(ushort port, string uriTemplate, Func<T1, T2, T3, T4, T5, Response> code, HandlerOptions ho = null)
         {
+            AddSelfOnlyOption(ref ho);
             Handle.GET(port, uriTemplate, code, ho);
         }
         #endregion
@@ -80,6 +93,15 @@ namespace Images.Helpers
         public static void Map(string appUriToMap, string mapUri, Func<string, string> converterTo, Func<string, string> converterFrom, string method)
         {
             UriMapping.Map(appUriToMap, mapUri, converterTo, converterFrom, method);
+        }
+
+        private static void AddSelfOnlyOption(ref HandlerOptions options)
+        {
+            if (options == null)
+            {
+                options = new HandlerOptions();
+            }
+            options.SelfOnly = true;
         }
         #endregion
         #region UriMapping.OntologyMap replacement
