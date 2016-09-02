8e52c4897a47442306efb48423b37da482ac557f
diliger <vyachdiliger@rambler.ru>
2016-09-02 19:09:19 +0300

    Images carousel to add more than one image for products
    half done
    the problem that removed image still appears in carousel

--- src/Images/Api/MainHandlers.cs
+++ src/Images/Api/MainHandlers.cs
@@ -1,24 +1,30 @@
 ﻿using Images.Helpers;
-﻿using System.Web.UI.WebControls;
+using System.Web.UI.WebControls;
 using Starcounter;
 using Simplified.Ring1;
 using Simplified.Ring3;
 using Simplified.Ring6;
 using Page = Starcounter.Page;
 
-namespace Images {
-    internal class MainHandlers {
-        public void Register() {
-            Handle.GET("/images/standalone", () => {
+namespace Images
+{
+    internal class MainHandlers
+    {
+        public void Register()
+        {
+            Handle.GET("/images/standalone", () =>
+            {
                 Session session = Session.Current;
 
-                if (session != null && session.Data != null) {
+                if (session != null && session.Data != null)
+                {
                     return session.Data;
                 }
 
                 StandalonePage standalone = new StandalonePage();
 
-                if (session == null) {
+                if (session == null)
+                {
                     session = new Session(SessionOptions.PatchVersioning);
                     standalone.Html = "/Images/viewmodels/StandalonePage.html";
                 }
@@ -28,8 +34,10 @@ namespace Images {
             });
 
             // Workspace root (Launchpad)
-            Handle.GET("/images", (Request request) => {
-                return Db.Scope<StandalonePage>(() => {
+            Handle.GET("/images", (Request request) =>
+            {
+                return Db.Scope<StandalonePage>(() =>
+                {
                     StandalonePage master = this.GetMaster();
 
                     master.CurrentPage = Self.GET("/images/partials/images");
@@ -38,8 +46,10 @@ namespace Images {
                 });
             });
 
-            Handle.GET("/images/image/{?}", (string objectId) => {
-                return Db.Scope<StandalonePage>(() => {
+            Handle.GET("/images/image/{?}", (string objectId) =>
+            {
+                return Db.Scope<StandalonePage>(() =>
+                {
                     StandalonePage master = this.GetMaster();
 
                     master.CurrentPage = Self.GET<IllustrationPage>("/Images/partials/image/" + objectId);
@@ -47,8 +57,10 @@ namespace Images {
                 });
             });
 
-            Handle.GET("/images/concept/{?}", (string objectId) => {
-                return Db.Scope<Json>(() => {
+            Handle.GET("/images/concept/{?}", (string objectId) =>
+            {
+                return Db.Scope<Json>(() =>
+                {
                     return Self.GET<ConceptPage>("/images/partials/concept/" + objectId);
                 });
             });
@@ -62,24 +74,31 @@ namespace Images {
             helper.DeleteOldFiles();
         }
 
-        protected StandalonePage GetMaster() {
+        protected StandalonePage GetMaster()
+        {
             return Self.GET<StandalonePage>("/images/standalone");
         }
 
-        protected void RegisterLauncherHooks() {
-            Handle.GET("/images/app-name", () => {
+        protected void RegisterLauncherHooks()
+        {
+            Handle.GET("/images/app-name", () =>
+            {
                 return new AppName();
             });
 
             // Menu
-            Handle.GET("/images/menu", () => {
+            Handle.GET("/images/menu", () =>
+            {
                 return new Page() { Html = "/Images/viewmodels/AppMenu.html" };
             });
         }
 
-        protected void RegisterPartials() {
-            Handle.GET("/images/partials/images", (Request request) => {
-                IllustrationsPage page = new IllustrationsPage() {
+        protected void RegisterPartials()
+        {
+            Handle.GET("/images/partials/images", (Request request) =>
+            {
+                IllustrationsPage page = new IllustrationsPage()
+                {
                     Html = "/Images/viewmodels/ImagesPage.html",
                     Uri = request.Uri
                 };
@@ -87,9 +106,12 @@ namespace Images {
                 return page;
             });
 
-            Handle.GET("/images/partials/image/{?}", (string objectId) => {
-                return Db.Scope<Json>(() => {
-                    var a = new IllustrationPage() {
+            Handle.GET("/images/partials/image/{?}", (string objectId) =>
+            {
+                return Db.Scope<Json>(() =>
+                {
+                    var a = new IllustrationPage()
+                    {
                         Html = "/Images/viewmodels/ImagePage.html",
                         Data = Db.SQL<Simplified.Ring1.Illustration>("SELECT o FROM Simplified.Ring1.Illustration o WHERE ObjectID=?", objectId).First
                     };
@@ -98,10 +120,13 @@ namespace Images {
                 });
             });
 
-            Handle.GET("/images/partials/concept/{?}", (string objectId) => {
-                return Db.Scope<Json>(() => {
+            Handle.GET("/images/partials/concept/{?}", (string objectId) =>
+            {
+                return Db.Scope<Json>(() =>
+                {
                     Something something = Db.SQL<Something>("SELECT o FROM Simplified.Ring1.Something o WHERE ObjectID = ?", objectId).First;
-                    ConceptPage a = new ConceptPage() {
+                    ConceptPage a = new ConceptPage()
+                    {
                         Html = "/Images/viewmodels/ConceptPage.html",
                         Data = something
                     };
@@ -110,16 +135,20 @@ namespace Images {
                 });
             });
 
-            Handle.GET("/images/partials/concept-somebody/{?}", (string objectId) => {
+            Handle.GET("/images/partials/concept-somebody/{?}", (string objectId) =>
+            {
                 return Self.GET("/images/partials/concept/" + objectId);
             });
 
-            Handle.GET("/images/partials/concept-vendible/{?}", (string objectId) => {
+            Handle.GET("/images/partials/concept-vendible/{?}", (string objectId) =>
+            {
                 return Self.GET("/images/partials/concept/" + objectId);
             });
 
-            Handle.GET("/images/partials/preview/{?}", (string objectId) => {
-                return Db.Scope<Json>(() => {
+            Handle.GET("/images/partials/preview/{?}", (string objectId) =>
+            {
+                return Db.Scope<Json>(() =>
+                {
                     Illustration img = DbHelper.FromID(DbHelper.Base64DecodeObjectID(objectId)) as Illustration;
                     PreviewPage page = new PreviewPage();
 
@@ -129,11 +158,13 @@ namespace Images {
                 });
             });
 
-            Handle.GET("/images/partials/preview-chatmessage/{?}", (string objectId) => {
+            Handle.GET("/images/partials/preview-chatmessage/{?}", (string objectId) =>
+            {
                 return Self.GET("/images/partials/preview/" + objectId);
             });
 
-            Handle.GET("/images/partials/preview-chatattachment/{?}", (string objectId) => {
+            Handle.GET("/images/partials/preview-chatattachment/{?}", (string objectId) =>
+            {
                 return Self.GET("/images/partials/preview/" + objectId);
             });
 
@@ -152,13 +183,15 @@ namespace Images {
                 return draft;
             });
 
-            Handle.GET("/images/partials/images/{?}", (string objectId) => {
+            Handle.GET("/images/partials/images/{?}", (string objectId) =>
+            {
                 var message = (Something)DbHelper.FromID(DbHelper.Base64DecodeObjectID(objectId));
                 var illustration = Db.SQL<Illustration>(@"Select m from Simplified.Ring1.Illustration m Where m.ToWhat = ?", message).First;
                 return illustration == null ? new Page() : Self.GET("/images/partials/preview/" + illustration.GetObjectID());
             });
 
-            Handle.GET("/images/partials/imageattachment/{?}", (string illustrationId) => {
+            Handle.GET("/images/partials/imageattachment/{?}", (string illustrationId) =>
+            {
                 return Db.Scope<Json>(() =>
                 {
                     var illustration = DbHelper.FromID(DbHelper.Base64DecodeObjectID(illustrationId)) as Illustration;
@@ -186,27 +219,31 @@ namespace Images {
             });
             #endregion
 
-            UploadHandlers.GET("/images/images", task => {
-                Session.ScheduleTask(task.SessionId, (s, id) => {
+            UploadHandlers.GET("/images/images", task =>
+            {
+                Session.ScheduleTask(task.SessionId, (s, id) =>
+                {
                     s.CalculatePatchAndPushOnWebSocket();
                 });
             });
             Handle.GET("/images/settings", () =>
             {
-                return new Transaction().Scope(() => {
+                return new Transaction().Scope(() =>
+                {
                     var page = new SettingsPage();
                     page.LoadDefaultData();
                     return page;
                 });
-            }, new HandlerOptions { SelfOnly =  true });
+            }, new HandlerOptions { SelfOnly = true });
         }
 
-        protected void RegisterMapperHandlers() {
+        protected void RegisterMapperHandlers()
+        {
 
             UriMapping.Map("/images/menu", UriMapping.MappingUriPrefix + "/menu");
             UriMapping.Map("/images/app-name", UriMapping.MappingUriPrefix + "/app-name");
             UriMapping.Map("/images/settings", UriMapping.MappingUriPrefix + "/settings");
-            
+
             UriMapping.OntologyMap("/images/partials/concept-somebody/@w", typeof(Somebody).FullName, null, null);
             UriMapping.OntologyMap("/images/partials/concept-vendible/@w", typeof(Product).FullName, null, null);
 
--- src/Images/ViewModels/ConceptPage.json.cs
+++ src/Images/ViewModels/ConceptPage.json.cs
@@ -1,5 +1,7 @@
 using Starcounter;
 using Simplified.Ring1;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Images
 {
@@ -10,8 +12,16 @@ namespace Images
 
         static ConceptPage()
         {
-            DefaultTemplate.ImageURL.Bind = nameof(URL);
-            DefaultTemplate.ImageMimeType.Bind = nameof(MimeType);
+            //DefaultTemplate.ImageURL.Bind = nameof(URL);
+            //DefaultTemplate.ImageMimeType.Bind = nameof(MimeType);
+            DefaultTemplate.Illustrations.Bind = nameof(ConceptIllustrations);
+
+            DefaultTemplate.Selected.MimeType.Bind = nameof(IllustrationPage.ContentMimeType);
+            DefaultTemplate.Selected.IsVideo.Bind = nameof(IllustrationPage.GetIsVideo);
+            DefaultTemplate.Selected.IsImage.Bind = nameof(IllustrationPage.GetIsImage);
+            DefaultTemplate.Selected.Name.Bind = nameof(IllustrationPage.ConceptName);
+            DefaultTemplate.Selected.MimeType.Bind = nameof(IllustrationPage.ContentMimeType);
+            DefaultTemplate.Selected.ImageURL.Bind = nameof(IllustrationPage.ContentURL);
         }
 
         public string MimeType
@@ -49,6 +59,8 @@ namespace Images
             }
         }
 
+        public IEnumerable<Illustration> ConceptIllustrations => Illustration.GetIllustrations(this.Data);
+
         protected override void OnData()
         {
             base.OnData();
@@ -59,24 +71,34 @@ namespace Images
             {
                 this.AllowedMimeTypes.Add().StringValue = s;
             }
-
-            this.SessionId = Session.Current.SessionId;
+            
+            //this.SessionId = Session.Current.SessionId;
+            this.Selected.Data = ConceptIllustrations.FirstOrDefault() ?? new Illustration() { Concept = this.Data, Content = new Content() }; ;
         }
 
         void Handle(Input.Delete action)
         {
-            if (this.Data.Illustration == null)
+            if (this.Selected == null)
             {
                 return;
             }
 
-            this.helper.DeleteFile(this.Data.Illustration);
-            this.Data.Illustration.Content?.Delete();
+            this.helper.DeleteFile(Selected.Data);
+            Selected.Data.Content?.Delete();
+            Selected.Data.Delete();
+            this.Transaction.Commit();
+
+            this.Selected.Data = ConceptIllustrations.FirstOrDefault() ?? new Illustration() { Concept = this.Data, Content = new Content() };
         }
 
         void Handle(Input.Save action)
         {
             this.Transaction.Commit();
+        }
+
+        void Handle(Input.Add action)
+        {
+            Selected.Data = new Illustration() { Concept = this.Data, Content = new Content() };
         }
 
         Illustration InitIllustration()
@@ -99,6 +121,68 @@ namespace Images
                 illustration.Content = new Content() { };
             }
             return illustration;
+        }
+
+        [ConceptPage_json.Illustrations]
+        partial class IllustrationPage : Page, IBound<Simplified.Ring1.Illustration>
+        {
+            public ConceptPage ParentPage => Parent.Parent as ConceptPage;
+            public bool GetIsImage => ParentPage.helper.IsImage(Data?.Content?.MimeType);
+            public bool GetIsVideo => ParentPage.helper.IsVideo(Data?.Content?.MimeType);
+
+            public string ConceptName
+            {
+                get
+                {
+                    return Data?.Concept?.Name;
+                }
+                set
+                {
+                    Data.Concept.Name = value;
+                }
+            }
+            public string ContentMimeType
+            {
+                get
+                {
+                    return Data?.Content?.MimeType;
+                }
+                set
+                {
+                    Data.Content.MimeType = value;
+                }
+            }
+
+            public string ContentURL
+            {
+                get
+                {
+                    return Data?.Content?.URL;
+                }
+                set
+                {
+                    Data.Content.URL = value;
+                }
+            }
+
+            static IllustrationPage()
+            {
+                DefaultTemplate.IsVideo.Bind = nameof(GetIsVideo);
+                DefaultTemplate.IsImage.Bind = nameof(GetIsImage);
+                DefaultTemplate.Name.Bind = nameof(ConceptName);
+                DefaultTemplate.MimeType.Bind = nameof(ContentMimeType);
+                DefaultTemplate.ImageURL.Bind = nameof(ContentURL);
+            }
+
+            protected override void OnData()
+            {
+                this.Url = string.Format("/images/image/{0}", this.Key);
+            }
+
+            void Handle(Input.Select action)
+            {
+                ParentPage.Selected.Data = this.Data;
+            }
         }
     }
 }
