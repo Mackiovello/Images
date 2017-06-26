ad0eae1ffed9226eeb8cb17a068b9ea0b39ccae0
Mihai <mihai@starcounter.com>
2017-06-26 03:25:20 -0700

    Pass along parameters to star (run.bat); for Starcounter/Guidelines#31

--- run.bat
+++ run.bat
@@ -2,4 +2,4 @@
 
 IF "%CONFIGURATION%"=="" SET CONFIGURATION=Debug
 
-star --resourcedir="%~dp0src\Images\wwwroot" "%~dp0src/Images/bin/%CONFIGURATION%/Images.exe"
+star %* --resourcedir="%~dp0src\Images\wwwroot" "%~dp0src/Images/bin/%CONFIGURATION%/Images.exe"
